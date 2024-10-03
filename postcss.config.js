03753e2788b389fe1fddc80d4b1bb027d03c4a09
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-10-03 18:15:57 +0700

    feat: initial commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    // 'tailwindcss/nesting': {},
+    // tailwindcss: {},
+    '@tailwindcss/postcss': {},
+    'postcss-preset-env': {},
+    cssnano: {},
+  },
+};
